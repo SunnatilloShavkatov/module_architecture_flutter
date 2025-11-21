81f7928a1c26627a0264ea44b38af17bb6d84ff5
Sunnatillo <sunnatilloshavkatov@gmail.com>
2025-11-21 17:44:18 +0500

    minor changes

--- modules/auth/lib/src/auth_container.dart
+++ modules/auth/lib/src/auth_container.dart
@@ -1,11 +1,13 @@
+import 'package:auth/src/di/auth_injection.dart';
+import 'package:auth/src/router/auth_router.dart';
 import 'package:core/core.dart';
 
 final class AuthContainer implements ModuleContainer {
   const AuthContainer();
 
   @override
-  Injection? get injection => null;
+  Injection? get injection => const AuthInjection();
 
   @override
-  AppRouter? get router => null;
+  AppRouter? get router => const AuthRouter();
 }
