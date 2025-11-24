9234d153d0160a6ec2de5a19880c62c8a159092c
Sunnatillo <sunnatilloshavkatov@gmail.com>
2025-11-24 17:51:39 +0500

    minor changes

--- packages/navigation/lib/navigation.dart
+++ packages/navigation/lib/navigation.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:navigation/src/observer/route_navigation_observer.dart';
 
 export 'package:go_router/go_router.dart';
-export 'src/custom_page_route/material_sheet_route.dart';
+export 'src/custom_page_route/material_sheet_page.dart';
 export 'src/name_routes.dart';
 export 'src/observer/route_navigation_observer.dart';
 
