59d40b1c4baa99f2b716d95445ea9024709ea051
Sunnatillo <sunnatilloshavkatov@gmail.com>
2026-02-26 14:23:19 +0500

    feat: update splash page to check login status and navigate accordingly

--- modules/initial/lib/src/presentation/splash/splash_page.dart
+++ modules/initial/lib/src/presentation/splash/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:components/components.dart';
+import 'package:core/core.dart';
 import 'package:flutter/material.dart';
 import 'package:navigation/navigation.dart';
 
@@ -10,14 +11,24 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+  LocalSource get _localSource => AppInjector.instance.get<LocalSource>();
+
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 1), () {
-      if (mounted) {
-        context.pushReplacementNamed(Routes.welcome);
-      }
-    });
+    _checkLoginStatus().ignore();
+  }
+
+  Future<void> _checkLoginStatus() async {
+    final bool isLoggedIn = await _localSource.hasProfile;
+    if (!mounted) {
+      return;
+    }
+    if (isLoggedIn) {
+      context.pushReplacementNamed(Routes.mainHome);
+    } else {
+      context.pushReplacementNamed(Routes.welcome);
+    }
   }
 
   @override
